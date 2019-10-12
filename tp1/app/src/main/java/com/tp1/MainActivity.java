87ed16000105ce10a08f6cd58c293b511a38585b
krakenfrancois <f.auxietre@gmail.com>
2019-10-12 18:01:25 +0200

    Add 2 Menus one in the first Activity and one in the uesr_Activity

--- tp1/app/src/main/java/com/tp1/MainActivity.java
+++ tp1/app/src/main/java/com/tp1/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +14,8 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button button_user;
+    private Menu menu = null;
+
 
     @Override
     /**
@@ -53,7 +57,12 @@ public class MainActivity extends AppCompatActivity {
      */
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        //R.menu.menu est l'id de notre menu
+        inflater.inflate(R.menu.menu_main, menu);
+
+       // getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -64,9 +73,7 @@ public class MainActivity extends AppCompatActivity {
      *
      */
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        //les actions sont definies dans les methodes ci dessous et dans le fichier menu_main
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -74,6 +81,34 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+
+        switch (id) {
+            case R.id.reset_form:
+                //Dans le Menu "m", on active tous les items dans le groupe d'identifiant "R.id.group2"
+                menu.setGroupEnabled(R.id.reset_form, true);
+                return true;
+
+            case R.id.add_form:
+                menu.setGroupEnabled(R.id.add_form, true);
+                return true;
+
+            default: {
+                throw new IllegalStateException("Inapropriate value please check in mainActivity: " + item.getItemId());
+            }
+
+        }
+
+
+        //return super.onOptionsItemSelected(item);
     }
+
+    public void reset_form(MenuItem item) {
+            Log.i("info", "methode resest form");
+//        ((TextInputLayout)findViewById(R.id.input_nom)).getEditText().setText("");
+//        ((TextInputLayout)findViewById(R.id.input_prenom)).getEditText().setText("");
+//        ((TextInputLayout)findViewById(R.id.input_ville)).getEditText().setText("");
+//        ((EditText)findViewById(R.id.input_dob)).setText("");
+    }
+
+
 }
