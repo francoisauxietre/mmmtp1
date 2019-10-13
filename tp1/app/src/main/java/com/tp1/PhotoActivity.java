0f49f47a296fc6f6961b17457c154c0f96bd3b4f
krakenfrancois <f.auxietre@gmail.com>
2019-10-13 11:48:08 +0200

    Add camera Acvtivity

---
+++ tp1/app/src/main/java/com/tp1/PhotoActivity.java
@@ -0,0 +1,16 @@
+package com.tp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+
+
+}
